4cc67b0ed7061d3fa789182d3741e2db06cbfeea
Pin Phearak <phearakroun@gmail.com>
2022-07-17 15:56:03 +0700

    Angular 14 Export to Excel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import * as XLSX from 'xlsx';
+import {formatDate} from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-excel';
+  date = formatDate(new Date(), 'yyyy/MM/dd', 'en');
+  fileName= 'Excel -'+this.date+'.xlsx';
+  
+  exportexcel(): void 
+    {
+       /* table id is passed over here */   
+       let element = document.getElementById('excel-table'); 
+       const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+       const wb: XLSX.WorkBook = XLSX.utils.book_new();
+       XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+       XLSX.writeFile(wb, this.fileName);
+			
+    }
 }
